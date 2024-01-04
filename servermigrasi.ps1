f8cfda94dc2f38882dff61344a4ad9cbf041af0d
uchihaobitos <155628753+uchihaobitos@users.noreply.github.com>
2024-01-04 17:10:56 +0700

    Update servermigrasi.ps1

--- servermigrasi.ps1
+++ servermigrasi.ps1
@@ -1,3 +1,4 @@
+# Pindah ke direktori Documents
 cd .\Documents
 
 # Membuat folder "Migrasi"
@@ -5,25 +6,22 @@ mkdir "Migrasi"
 cd .\Migrasi
 
 # Mengunduh dan menginstal Google Chrome
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile "$env:USERPROFILE\Downloads\chrome_installer.exe"
 Start-Process -FilePath "$env:USERPROFILE\Downloads\chrome_installer.exe" -Wait
 Start-Process -FilePath "chrome.exe" -ArgumentList "http://cloudm.local"
 
 # Mengunduh dan menginstal WinRAR
-Invoke-WebRequest -Uri "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-624.exe" -OutFile "winrar-x64-624.exe"
-Start-Process -FilePath ".\winrar-x64-624.exe" -Wait
+Invoke-WebRequest -Uri "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-624.exe" -OutFile "$env:USERPROFILE\Downloads\winrar-x64-624.exe"
+Start-Process -FilePath "$env:USERPROFILE\Downloads\winrar-x64-624.exe" -Wait
 
 # Mengunduh dan menginstal AnyDesk secara diam-diam
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri "https://download.anydesk.com/AnyDesk.exe" -OutFile "$env:USERPROFILE\Downloads\AnyDesk.exe"
 Start-Process -FilePath "$env:USERPROFILE\Downloads\AnyDesk.exe" -ArgumentList "/VERYSILENT" -Wait
 
-# Mengunduh dan menjalankan Google Chrome lagi
-Invoke-WebRequest -Uri "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile "$env:USERPROFILE\Downloads\chrome_installer.exe"
-Start-Process -FilePath "$env:USERPROFILE\Downloads\chrome_installer.exe" -Wait
-Start-Process -FilePath "chrome.exe" -ArgumentList "http://cloudm.local"
-
-# Mengatur protokol keamanan dan mengunduh serta menjalankan CloudM-Migrate
+# Mengatur protokol keamanan untuk Tls12 lagi sebelum mengunduh CloudM-Migrate
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Mengunduh dan menjalankan CloudM-Migrate
 Invoke-WebRequest -Uri "https://storage.googleapis.com/cloudmigrator/CloudM-Migrate-3.42.23.0-x64.exe" -OutFile "$env:USERPROFILE\Downloads\CloudM-Migrate-3.42.23.0-x64.exe"
 Start-Process -FilePath "$env:USERPROFILE\Downloads\CloudM-Migrate-3.42.23.0-x64.exe" -Wait
